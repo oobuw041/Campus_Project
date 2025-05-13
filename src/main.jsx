6ea92ab2f36189f574d35832170248d268c58e15
keno <keno.obuwoma@gmail.com>
2025-05-13 18:19:13 -0400

    boss level bug slayed

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Campus_Project">
       <App />
     </BrowserRouter>
   </StrictMode>
